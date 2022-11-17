0ad634d457c274b809776a86f50494869d5b4bc7
cobalt268 <cobaltisacoolore@gmail.com>
2022-11-17 17:00:38 -0600

    updated javadoc

--- nws-doc/package-search-index.js
+++ nws-doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"weather"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"nwsWeatherApi"}];updateSearchResults();
